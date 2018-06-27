f8d63b956fc969629b676c107b150414136258d5
Naod Ayele <naodakililu@yahoo.com>
2018-06-27 18:51:18 -0400

    refcode files

---
+++ Refcode-master/Sprint-02/guessing-game.js
@@ -0,0 +1,66 @@
+// GUESSING GAME!
+// Below you will find some sections of code with one or more questions about that code.
+// Write your answer to each question on the line after A:
+// You don't have to know the answer, but you do have to guess!
+
+x = 5;
+
+// Q: In the code above, what is the value of `x`?
+// A:
+
+myVariable = 23;
+
+// Q: In the code above, what is the value of `myVariable`?
+// A:
+
+a = 23;
+b = 45;
+d = 99;
+
+// Q: In the code above, what is the value of `c`?
+// A:
+
+foo = 1;
+bar = 2;
+baz = 3;
+qux = 4;
+
+// Q: In the code above, what is the name of the variable that has the value 3?
+// A:
+
+thud = 5;
+thud = 1;
+
+// Q: In the code above, what is the value of `thud`?
+// A:
+
+brenton = -100;
+brenton = 5.5;
+brenton = 0;
+dinosaur = -200;
+dinosaur = 8;
+dinosaur = 999999;
+
+// Q: In the code above, what is the value of `dinosaur`?
+// A:
+// Q: In the code above, all of the variables above are "number" type. What do you think are the possible values of the number type?
+// A:
+
+x = 1;
+y = x;
+
+// Q: In the code above, what is the value of `x`?
+// A:
+// Q: What is the value of `y`?
+// A:
+// Q: Are `x` and `y` the same, or are they different?
+// A:
+
+one = 1;
+two = 2;
+three = 3;
+five = 5;
+six = 6;
+
+// Q: What is the value of `four`?
+// A:
---
+++ Refcode-master/Sprint-02/guessing-game2.js
@@ -0,0 +1,58 @@
+// GUESSING GAME!
+// Below you will find some sections of code with one or more questions about that code.
+// Write your answer to each question on the line after A:
+
+someWords = "This is some words!";
+
+// Q: What is the value of `someWords`?
+// A:
+
+aNumber = 4;
+aString = "4";
+
+// Q: What is the value of `aNumber`? What makes it different from `aString`?
+// A:
+
+aString = "true";
+aBoolean = true;
+
+// Q: What is the value of `aBoolean`? What makes it different from `aString`?
+// A:
+
+question0 = true;
+question1 = false;
+question2 = true;
+question3 = false;
+question4 = false;
+question5 = true;
+question6 = true;
+
+// All of the variables above have a value that is "boolean" type.
+// Q: What do you think are the possible values of a boolean type variable?
+// A:
+
+foo = true;
+bar = "true"
+baz = 1;
+
+// Q: What is the name of the variable that is a "string" type?
+// A:
+// Q: What is the name of the variable that is a "number" type?
+// A:
+// Q: What is the name of the variable that is a "boolean" type?
+// A:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+foo, bar, baz, qux, quux, corge, uier, grault, garply, waldo, fred, plugh, thud, mos, henk, def.
